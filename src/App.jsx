c253938ab96db30a2e0151d276c32165876c9a4e
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2025-09-02 15:28:04 +0000

    caching added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,20 @@ import { useEffect } from "react";
 export default function App() {
   const [results, setResults] = useState([]);
   const [input, setInput] = useState("");
+  const [cache, setCache] = useState({});
 
   const fetchdata = async () => {
-    console.log(input);
+    if (cache[input]) {
+      console.log("from cache");
+      setResults(cache[input]);
+      return;
+    }
+
+    console.log("from API");
     const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
     const res = await data.json();
     setResults(res?.recipes);
+    setCache((prev) => ({ ...prev, [input]: res?.recipes }));
   };
 
   useEffect(() => {
